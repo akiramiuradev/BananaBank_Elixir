57529079f061d4a03799bc75fa28cae7929a9544
Akira Miura <guilhermemiura77@gmail.com>
2026-03-04 17:19:27 -0300

    creating test for users_controller

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -34,6 +34,7 @@ defmodule BananaBank.Users.User do
     |> validate_length(:name, min: 3)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:cep, is: 8)
+    |> validate_length(:password, min: 8)
   end
 
   defp add_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
---
+++ test/banana_bank_web/controllers/users_controller_test.exs
@@ -0,0 +1,93 @@
+defmodule BananaBankWeb.UsersControllerTest do
+  use BananaBankWeb.ConnCase
+
+  alias BananaBank.Users
+  alias Users.User
+
+  describe "create/2" do
+    test "successfully creates an user", %{conn: conn} do
+
+      params = %{
+        name: "joão",
+        cep: "12345678",
+        email: "joao@joao.com",
+        password: "12345678"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:created)
+
+      assert %{
+              "data" => %{
+                "cep" => "12345678",
+                "email" => "joao@joao.com",
+                "id" => _id,
+                "name" => "joão"
+              },
+              "message" => "User criado com sucesso"
+            } = response
+
+    end
+
+    test "when there are invalid params, returns an error", %{conn: conn} do
+
+      params = %{
+        name: "joão",
+        cep: "12",
+        email: "joaojoao.com",
+        password: "123456"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:bad_request)
+
+
+      expected_response = %{
+              "errors" => %{
+              "cep" => ["should be 8 character(s)"],
+              "email" => ["has invalid format"],
+              "password" => ["should be at least 8 character(s)"]
+              }
+            }
+
+      assert response == expected_response
+
+    end
+  end
+
+  describe "delete/2" do
+    test "sucessfully deletes an user", %{conn: conn} do
+
+    params = %{
+        name: "joão",
+        cep: "12345678",
+        email: "joao@joao.com",
+        password: "12345678"
+      }
+
+    {:ok, %User{id: id}} = Users.create(params)
+
+    response =
+      conn
+      |> delete(~p"/api/users/#{id}")
+      |> json_response(:ok)
+
+    expected_response = %{
+              "data" => %{
+                "cep" => "12345678",
+                "email" => "joao@joao.com",
+                "id" => id,
+                "name" => "joão"
+              },
+              "message" => "User deletado com sucesso"
+            }
+
+    assert response == expected_response
+
+    end
+  end
+end
